39738517ba01ec187bb76939ae7c3a3c511a43dc
Dima <wildface.w@gmail.com>
2021-08-24 19:18:15 +0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ms from '.bin/ms.module.scss'
+const Header = () => {
+    return <div className={ms.block_container}>
+            <p>asd</p>
+    </div>
+}
+export default Header;
